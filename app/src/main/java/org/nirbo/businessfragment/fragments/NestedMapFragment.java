5faceb54f2a385452c32bfa6e23c2f5586fa87f8
nirbo <nirbo2@gmail.com>
2014-11-29 04:28:32 +0200

    Camera will now show the device's current location based on Network_Provider

--- app/src/main/java/org/nirbo/businessfragment/fragments/NestedMapFragment.java
+++ app/src/main/java/org/nirbo/businessfragment/fragments/NestedMapFragment.java
@@ -1,14 +1,25 @@
 package org.nirbo.businessfragment.fragments;
 
+import android.app.Activity;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class NestedMapFragment extends MapFragment {
+
+    static NestedMapFragment fragment;
 
     // Default constructor
     public NestedMapFragment() {
@@ -17,7 +28,7 @@ public class NestedMapFragment extends MapFragment {
 
     // newInstance method to instantiate new fragments
     public static NestedMapFragment newInstance() {
-        NestedMapFragment fragment = new NestedMapFragment();
+        fragment = new NestedMapFragment();
         return fragment;
     }
 
@@ -42,6 +53,30 @@ public class NestedMapFragment extends MapFragment {
         settings.setRotateGesturesEnabled(false);
         settings.setMyLocationButtonEnabled(false);
         settings.setZoomControlsEnabled(false);
+
+        displayCurrentLocation(fragment);
+    }
+
+    private void displayCurrentLocation(MapFragment fragment) {
+        Activity context = getActivity();
+        LocationManager locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+        Location location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+
+        if (location != null) {
+            LatLng coordinates = new LatLng(location.getLatitude(), location.getLongitude());
+            GoogleMap mMap = fragment.getMap();
+
+            MarkerOptions markerOptions = new MarkerOptions()
+                    .position(coordinates);
+
+            CameraPosition cameraPosition = new CameraPosition.Builder()
+                    .target(coordinates)
+                    .zoom(15)
+                    .build();
+
+            mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+            mMap.addMarker(markerOptions);
+        }
     }
 
 }
